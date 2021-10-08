30db3e6252b3fdb18a82cb94de921c7b9953eaf0
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-10-08 11:20:27 -0500

    RootedUp

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if given node is root
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is root, 0 if NULL or otherwise
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	
+	if ((!(node->left)) && (!(node->right)))
+		reutrn (1);
+
+	else
+		return (0);
+}
