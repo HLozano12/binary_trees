261ba78dc5305e536ce90ef3d0c7796b33e907c4
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-10-16 17:43:52 -0500

    Sibling format change

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -13,11 +13,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if( node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
+	if (node->parent->right == NULL)
+		return (NULL);
+	else if (node->parent->left->n == node->n)
 		return (node->parent->right);
-
-	if (node->parent->right == node)
+	else
 		return (node->parent->left);
-
-	return (NULL);
 }
