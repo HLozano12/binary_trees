18e3c9ee0658404b54cbf03f45ecdfdf47c3c392
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-10-16 17:49:12 -0500

    C++ used mistake

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -13,10 +13,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if ((node == NULL) || (node->parent == NULL))
 		return (NULL);
 
-	if (node->parent->right == NULL)
-		return (NULL);
-	else if (node->parent->left->n == node->n)
+	if (node->parent->left == node)
 		return (node->parent->right);
-	else
+
+	if (node->parent->right == node)
 		return (node->parent->left);
+
+	return (NULL);
 }
